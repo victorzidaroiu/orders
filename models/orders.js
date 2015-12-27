7b5882b3794b7319995da414553cf28e8ea4182f
Victor Zidaroiu <victor.zidaroiu@icona.it>
2015-12-27 21:50:12 +0200

    Implemented the API.

--- models/orders.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var schema = new mongoose.Schema({
-	companyName: String,
-	customerAddress: String,
-	orderedItem: String
-});
-
-module.exports = mongoose.model('Orders', schema);
