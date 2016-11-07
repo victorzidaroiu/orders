7571fbef953e147a815e2e55eb75a4a2a007b0f3
Victor Zidaroiu <victor@zidaroiu.com>
2016-11-07 22:08:38 +0000

    Fix tests and linting

--- tests/integration/api.js
+++ tests/integration/api.js
@@ -1,74 +1,93 @@
 /* global describe, it */
 
-
+import 'babel-polyfill';
 import restler from 'restler';
 import dotenv from 'dotenv';
 import _debug from 'debug';
-const debug = _debug('api-test');
-dotenv.config({ silent: true })
+import { expect } from 'chai';
+/* eslint-disable */
+import server from '../../src/server';
+/* eslint-enable */
+const debug = _debug('api');
+dotenv.config({ silent: true });
 
 const serverUrl = `http://localhost:${process.env.PORT}`;
+let lastInsertedId = null;
 
-var lastInsertedId = null;
-describe('Orders API', function() {
-	this.timeout(5000);
-	it('should get the list of orders', () => {
-		restler.get(`${serverUrl}/api/orders`).on('success', (data, response) => {
-			debug('Returned Data: ');
-			debug(data);
-			if (data instanceof Array) {
-				done();
-      }
-		});
-	});
+debug(`Testing app on ${serverUrl}`);
 
-	it('should get the list of top orders', () => {
-		restler.get(server + '/api/top-orders').on('success', (data, response) => {
-			debug('Returned Data: ');
-			debug(data);
-			if (data instanceof Array)
-				done();
-		});
-	});
-
-	it('should insert an order and return the id', () => {
-		restler.post(server + '/api',{
-			data: {
-				companyName: 'companyName',
-				customerAddress: 'test customerAddress',
-				orderedItem: 'test orderedItem'
-			}
-		}).on('success', (data, response) => {
-			debug('Returned Data: ');
-			debug(data);
-			if (data && data._id) {
-				lastInsertedId = data._id;
-				done();
-			}
-		});
-	});
-
-	it('should find orders', () => {
-		restler.get(server + '/api/search/search-term').on('success', (data, response) => {
-			debug('Returned Data: ');
-			debug(data);
-			if (data instanceof Array) {
+describe('Orders API', () => {
+  it('should get the list of orders', (done) => {
+    restler.get(`${serverUrl}/api/orders/list`).on('success', (data) => {
+      if (data instanceof Array) {
         done();
       }
-		});
-	});
+    });
+  });
 
-	it('should delete an order', () => {
-		if (!lastInsertedId) {
-      return;
-    }
-
-		restler.del(`${server}/api/lastInsertedId`).on('success', (data, response) => {
-			debug('Returned Data: ');
-			debug(data);
-			if (data && data._id) {
+  it('should get the list of top orders', (done) => {
+    restler.get(`${serverUrl}/api/orders/top`).on('success', (data) => {
+      if (data instanceof Array) {
         done();
       }
-		});
-	});
+    });
+  });
+
+  it('should insert an order and return the id', (done) => {
+    restler.post(`${serverUrl}/api/orders/create`, {
+      data: {
+        companyName: 'Company name',
+        customerAddress: 'Customer address',
+        orderedItem: 'Item name',
+      },
+    }).on('success', (data) => {
+      /* eslint-disable */
+      expect(typeof data._id).to.equal('string');
+      lastInsertedId = data._id;
+      done();
+      /* eslint-enable */
+    });
+  });
+
+  it('should find orders by item name', (done) => {
+    const toSearch = 'Item name';
+    restler.get(`${serverUrl}/api/orders/search/${toSearch}`).on('success', (data) => {
+      expect(data instanceof Array).to.equal(true);
+      expect(data[0].orderedItem).to.equal(toSearch);
+      done();
+    });
+  });
+
+  it('should find orders by company name', (done) => {
+    const toSearch = 'Company name';
+    restler.get(`${serverUrl}/api/orders/search/${toSearch}`).on('success', (data) => {
+      expect(data instanceof Array).to.equal(true);
+      expect(data[0] instanceof Object).to.equal(true);
+      expect(data[0].companyName).to.equal(toSearch);
+      done();
+    });
+  });
+
+  it('should find orders by customer address', (done) => {
+    const toSearch = 'Customer address';
+    restler.get(`${serverUrl}/api/orders/search/${toSearch}`).on('success', (data) => {
+      expect(data instanceof Array).to.equal(true);
+      expect(data[0] instanceof Object).to.equal(true);
+      expect(data[0].customerAddress).to.equal(toSearch);
+      done();
+    });
+  });
+});
+
+describe('Orders API', () => {
+  it('should delete previously inserted item', (done) => {
+    expect(lastInsertedId).to.not.equal(null);
+
+    restler.del(`${serverUrl}/api/orders/delete/${lastInsertedId}`).on('success', (data) => {
+      /* eslint-disable */
+      expect(typeof data._id).to.equal('string');
+      /* eslint-enable */
+      done();
+    });
+  });
 });
